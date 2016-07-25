0374698b97f3df07861d8fe6cf0131af361a93c6
Robert Lazarony <robert_lazarony@abercrombie.com>
2016-07-24 21:33:36 -0400

    making create user more react-y

--- src/components/CreateAccount.js
+++ src/components/CreateAccount.js
@@ -2,34 +2,52 @@ import React from 'react'
 
 
 export default class CreateAccount extends React.Component {
+    
+    constructor (props) {
+        super(props);
+        this.state = {
+            name: '',
+            email: '',
+            password: ''
+        };
+        this.render = this.render.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+        this.addUserToStorage = this.addUserToStorage.bind(this);
+    }
+    
+    render() {
+        return (
+            <div>
+                Name: <input type="text" id="name" onChange={this.handleInputChange} value={this.state.name} placeholder="name (required)"/>
+                Email: <input type="email" id="email" onChange={this.handleInputChange} value={this.state.email} placeholder="email (required)"/>
+                Password: <input type="password" id="password" onChange={this.handleInputChange} value={this.state.password} placeholder="*********"/>
+                <button onClick={this.handleClick} disabled={!this.state.name || !this.state.email || !this.state.password}>Create</button>
+            </div>
+        );
+    };
+    
+    handleInputChange(event) {
+        var newState = {};
         
-        render() {
-            var self = this;
-            return (
-                <div>
-                    Name: <input type="text" id="name" />
-                    Email: <input type="email" id="email" />
-                    Password: <input type="password" id="password" />
-                    <button onClick={self.handleClick}>Create</button>
-                </div>
-            );
+        newState[event.target.id] = event.target.value;
+        this.setState(newState);
+    }
+    
+    handleClick () {
+        var user = {
+            name: this.state.name,
+            email: this.state.email,
+            password: this.state.password
         };
         
-        handleClick (e) {
-            e.preventDefault();
-            var newUser = {};
-            
-            newUser.name = $('#name').val();
-            newUser.email = $('#email').val();
-            newUser.password = $('#password').val();
-            
-            this.addUserToStorage(newUser);
-        };
+        this.addUserToStorage(user);
+    };
+    
+    addUserToStorage (user) {
+        var users = localStorage.getItem('users') || [];
         
-        addUserToStorage (user) {
-            var users = JSON.stringify(localStorage.getItem('users')) || [];
-            
-            users.push(user);
-            localStorage.setItem('users', JSON.stringify(users));
-        };
+        users.push(user);
+        localStorage.setItem('users', JSON.stringify(users));
+    };
 }
