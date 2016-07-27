40c597f5453a32d1d088bb8a7403448192d89c74
holden_profit <holdenprofit@gmail.com>
2016-07-27 17:34:36 -0400

    Removed popup dep., added Login.

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import { Router, Route, Link, browserHistory, RouteHandler } from 'react-router'
 import MainLayout from './components/MainLayout';
 import Splash from './components/Splash';
 import CreateAccount from './components/CreateAccount';
+import Login from './components/Login';
 
 render(
   <Router history={browserHistory}>
     <Route component={MainLayout}>
     <Route path="/" component={Splash} />
     <Route path="/CreateAccount" component={CreateAccount} />
+    <Route path="/Login" component={Login} />
   </Route>
 </Router>
 , document.getElementById('app')
