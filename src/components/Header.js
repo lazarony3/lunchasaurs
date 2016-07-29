7f073f4bae71c98dcd9d6d9d555eb5e0bd4ba8a6
Robert Lazarony <robert_lazarony@abercrombie.com>
2016-07-28 23:48:44 -0400

    logged in user now sees 'My Account' and 'logoff' in the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,22 +5,48 @@ import {Link} from 'react-router';
 export default class Header extends React.Component {
   constructor(props) {
     super(props);
+    
+    this.state = {loggedIn: false};
+    
     this.render = this.render.bind(this);
+    this.logoff = this.logoff.bind(this);
   }
   
   componentDidMount () {
+      var self = this;
       window.ee.addListener('loggedIn', function () {
           console.log('logged in');
+          self.setState({
+              loggedIn: true
+          });
       })
   }
   
   render() {
+      var loginLink, accountLink;
+      
+      loginLink = <Link to="/Login"> Login</Link>;
+      accountLink = <Link to="/CreateAccount"> Create Account</Link>
+
+      if (this.state.loggedIn) {
+          loginLink = <a href="#" onClick={this.logoff}>Logoff</a>
+          accountLink = <Link to="/MyAccount">My Account</Link>
+      }
+      
     return (
       <div class="header">
+      
         <Link to="/">Home</Link> |
-        <Link to="/CreateAccount"> Create Account</Link> |
-        <Link to="/Login"> Login</Link>
+        {accountLink} |
+        {loginLink}
       </div>
     );
+  }
+  
+  logoff (e) {
+      e.preventDefault()
+      this.setState({
+          loggedIn: false
+      });
   }
 }
