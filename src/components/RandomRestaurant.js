c91ccf2193dffa053e632eaf1a4b4df5b2d9e643
Robert Lazarony <robert_lazarony@abercrombie.com>
2016-07-31 21:10:51 -0400

    refactoring random restaurant to be a component

---
+++ src/components/RandomRestaurant.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import NavLink from './NavLink'
+
+export default class Preferences extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedRestaurant: false
+    };
+    this.render = this.render.bind(this);
+    this.pickRandomRestaurant = this.pickRandomRestaurant.bind(this);
+  }
+  render() {
+    return (
+        <div>
+            <div>Or if you are feeling lucky?<button onClick={this.pickRandomRestaurant}>Get to Lunch</button></div>
+            <div style={this.state.selectedRestaurant ? {display: 'block'} : {display: 'none'}}>You should get {this.state.selectedRestaurant}</div>           
+        </div>
+    );
+  }
+  
+  pickRandomRestaurant(e) {
+      var restaurants = ['Qdoba', 'City Barbeque', 'Jason\'s Deli', 'Manaas', 'Schmitd\'s'],
+        selectedIndex = Math.floor(Math.random() * restaurants.length);
+        
+        this.setState({
+            selectedRestaurant: restaurants[selectedIndex]
+        });  
+  }
+}
--- src/components/UserAccount.js
+++ src/components/UserAccount.js
@@ -2,18 +2,17 @@ import React from 'react'
 import {Link, browserHistory} from 'react-router'
 import Header from './Header.js'
 import FoodTypeSelectionList from './FoodTypeSelectionList.js'
+import RandomRestaurant from './RandomRestaurant'
 
 export default class UserAccount extends React.Component {
   constructor(props) {
     super(props);
     let user = JSON.parse(sessionStorage.getItem('user'));
     this.state = {
-      user: user,
-      selectedRestaurant: ''
+      user: user
     };
     this.render = this.render.bind(this);
     this.navigateToPrefs = this.navigateToPrefs.bind(this);
-    this.pickRandomRestaurant = this.pickRandomRestaurant.bind(this);
   }
   render() {
       var preferencesText = ''
@@ -24,8 +23,7 @@ export default class UserAccount extends React.Component {
         <div>
             <h2>Welcome {this.state.user.name}!</h2> 
             <div>{preferencesText}</div>
-            <div>Or if you are feeling lucky?<button onClick={this.pickRandomRestaurant}>Get to Lunch</button></div>
-            <div style={this.state.selectedRestaurant ? {display: 'block'} : {display: 'none'}}>You should get {this.state.selectedRestaurant}</div>   
+            <RandomRestaurant/>
         </div>
     );
   }
@@ -34,13 +32,5 @@ export default class UserAccount extends React.Component {
       e.preventDefault();
       this.state.user.preferencesSet = true;
       browserHistory.push('/preferences/cuisine') ;
-  }
-  pickRandomRestaurant(e) {
-      var restaurants = ['Qdoba', 'City Barbeque', 'Jason\'s Deli', 'Manaas', 'Schmitd\'s'],
-        selectedIndex = Math.floor(Math.random() * restaurants.length);
-        
-        this.setState({
-            selectedRestaurant: restaurants[selectedIndex]
-        });  
   }
 }
